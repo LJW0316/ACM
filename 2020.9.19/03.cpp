49cd3d19de88ae36c3f0a93ff8db92529a22402f
Lijingwei <lijingwei010316@163.com>
2020-09-19 21:42:11 +0800

    2020.9.19

--- 2020.9.19/03.cpp
+++ 2020.9.19/03.cpp
@@ -11,18 +11,17 @@ int main()
         a = num[0] * 100 + num[1] * 10 + num[2];              //a取数组前三元素组成的数
         b = num[3] * 100 + num[4] * 10 + num[5];              //b取数组中间三元素组成的数
         c = num[6] * 100 + num[7] * 10 + num[8];              //c取数组后三位元素组成的数
-        if (a * b * z == a * y * c && a * b * z == x * b * c) //a:b:c=x:y:z判断
+        if (a * y * z == b * x * z && a * y * z == c * x * y) //a:b:c=x:y:z判断
         {
             cnt++;
             cout << a << " " << b << " " << c << endl;
         }
 
-    } while (next_permutation(num, num + 9)); 
+    } while (next_permutation(num, num + 9));
     //使用next_permutation函数枚举num中元素所有顺序(SLT中函数，实现把123456789变换为12345698，当变换为与第一次相同的顺序时返回false)
     if (!cnt) //0种可能输出NO!!!
     {
-        cout << "NO!!!" << endl;
+        cout << "No!!!" << endl;
     }
-    cin >> x >> y >> z;
     return 0;
 }
